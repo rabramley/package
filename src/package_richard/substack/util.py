966f191d7a2f0de57b328a9101bd184b5ee66e59
Richard Bramley <rabramley@gmail.com>
2024-07-31 15:00:08 +0100

    Use a dependency

--- src/package_richard/substack/util.py
+++ src/package_richard/substack/util.py
@@ -1,2 +1,9 @@
+from datetime import *
+from dateutil.relativedelta import *
+
 def print_hey():
     print('Hey!')
+
+def print_date():
+    now = datetime.now()
+    print(now + relativedelta(months=1))
